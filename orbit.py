05633823071d357992b6a462838ffb9f67eb87b8
Misingnoglic <arya@aryaboudaie.com>
2014-04-12 15:47:22 -0400

    Fixed types
    fixed the types of elements

--- orbit.py
+++ orbit.py
@@ -2,19 +2,19 @@ class Orbit:
     def __init__(self,n,s1,s2):
         #first line
         self.name = n
-        self.number = s1[2:7]
+        self.number = int(s1[2:7])
         self.classification = s1[7]
-        self.internationaldesignatoryr = s1[9:11]
+        self.internationaldesignatoryr = int(s1[9:11])
         self.internationaldesignatorln = s1[11:14]
         self.internationaldesignatorpiece = s1[14:17]
         self.epochyear = s1[17:20]
-        self.epoch = s1[20:32]
-        self.firsttimederivativeofmean = s1[33:43]
+        self.epoch = float(s1[20:32])
+        self.firsttimederivativeofmean = float(s1[33:43])
         self.secondtimederivativeofmean = s1[44:52]
         self.bstardragterm = s1[53:61]
-        self.zero = s1[62:63]
+        self.zero = int(s1[62:63])
         self.elemsetnum = s1[64:68]
-        self.checksum = s1[68]
+        self.checksum = int(s1[68])
 
         #second line
         self.number2 = s2[2:7]
@@ -28,6 +28,7 @@ class Orbit:
         self.checksum = s2[68]
 
     def printstuff(self):
+        #To test the fields
         print [self.name,self.number,self.classification,self.internationaldesignatoryr,
                self.internationaldesignatorln,self.internationaldesignatorpiece,self.epochyear,
                self.epoch,self.firsttimederivativeofmean,self.secondtimederivativeofmean,self.bstardragterm,
