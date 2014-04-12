70242e998c9168a3d6967bed520b54addfe5bfcf
Misingnoglic <arya@aryaboudaie.com>
2014-04-12 15:05:34 -0400

    First Commit!
    Created a class that will parse data out of a Two-line element set to
    create an orbit

---
+++ orbit.py
@@ -0,0 +1,35 @@
+class Orbit:
+    def __init__(self,n,s1,s2)
+        #first line
+        self.name = n
+        self.number = s1[2:7]
+        self.classification = s1[7]
+        self.internationaldesignatoryr = s1[9:11]
+        self.internationaldesignatorln = s1[11:14]
+        self.internationaldesignatorpiece = s1[14:17]
+        self.epochyear = s1[18:20]
+        self.epoch = s1[20:32]
+        self.firsttimederivativeofmean = s1[33:43]
+        self.secondtimederivativeofmean = s1[44:52]
+        self.bstardragterm = s1[53:61]
+        self.zero = s1[62:63]
+        self.elemsetnum = s1[64:68]
+        self.checksum = s1[68]
+
+        #second line
+        self.number2 = s2[2:7]
+        self.inclination = s2[8:16] #degrees
+        self.rightascention = s2[17:25] #degrees
+        self.eccentricity = s2[26:33]
+        self.argumentofperigee = s2[34:42]
+        self.meananomaly = s2[43:51] #degrees
+        self.meanmotion = s2[52:53] 
+        self.revnumberatepoch = s2[63:68]
+        self.checksum = s2[68]
+
+    def printstuff(self):
+        print [self.name,self.number,self.classification,self.internationaldesignatoryr,self.internationaldesignatorln,self.epochyear,
+               self.epoch,self.firsttimederivativeofmean,self.secondtimederivativeofmean,self.bstardragterm,self.zero,self.elemsetnum, self.checksum]
+               
+        
+        
